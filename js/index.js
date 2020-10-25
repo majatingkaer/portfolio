aa09032515e35d23706aec5121daa23066b0e413
majatingkaer <maja.tingkaer@gmail.com>
2020-10-25 12:10:48 +0100

    til top pil indsat med js/gsap animation

--- js/index.js
+++ js/index.js
@@ -20,40 +20,15 @@ sections.forEach((section) => {
 });
 
 
-gsap.to(".slideInleft", {
-    translateX: 200,
+gsap.from(".topknap", {
+    opacity: 0,
+    y: 100,
     duration: 2,
-})
-
-
-gsap.from(".tegning", {
-    yPercent: 20,
-    stagger: .1,
-    ease: "none",
     scrollTrigger: {
-        trigger: ".tegning",
-        start: "top 90%",
-        end: "top 50%",
-        scrub: true,
+        trigger: ".topknap",
         //markers: true,
+        start: "bottom 25%",
+        end: "bottom",
+        scrub: true,
     }
-});
-
-
-gsap.to(".splash_flasker", {
-    translateX: 200,
-    duration: 1.5,
-    ease: "back.out(1.7)",
-});
-
-
-window.onscroll = function () {
-    scrollIndicator()
-};
-
-function scrollIndicator() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("Progress").style.width = scrolled + "%";
-}
+})
